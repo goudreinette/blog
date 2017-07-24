506de14d3f9a399b17717e07580b287c6fefef97
reinvdwoerd <reinvanderwoerd@me.com>
2017-07-24 13:11:41 +0200

    slim

--- config.rb
+++ config.rb
@@ -2,14 +2,15 @@
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 require 'stamp'
 
-activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
-end
+# activate :autoprefixer do |prefix|
+#   prefix.browsers = "last 2 versions"
+# end
 
 activate :blog do |blog|
   # set options on blog
    blog.sources = "posts/{year}-{month}-{day}-{title}"
    blog.permalink = "blog/{year}/{month}/{day}/{title}.html"
+   blog.layout = 'post'
 end
 
 # Layouts
